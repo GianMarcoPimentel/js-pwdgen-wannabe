29e796546e77644c2757f0af8a5edb49613fd2f9
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-25 16:11:23 +0100

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-prompt("Qual è il tuo nome?")
+let nome = prompt("Qual è il tuo nome?");
 
-prompt("Qual è il tuo cognome?")
+document.getElementById("name").innerHTML = `Il mio nome è ${nome}`;
 
-prompt("Qual è il tuo colore preferito?")
+let cognome = prompt("Qual è il tuo cognome?");
+
+document.getElementById("surname").innerHTML = `Il mio cognome è ${cognome}`;
+
+let colore = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("color").innerHTML = `Il  mio colore preferito è ${colore}`;
+
+document.getElementById("password").innerHTML = `La password è ${nome}${cognome}${colore}69`
